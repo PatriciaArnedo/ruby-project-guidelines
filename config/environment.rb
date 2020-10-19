35a8e71b6bd6c8ca643bd77d07ea710c8f1476c5
PatriciaArnedo <paaarnedo@gmail.com>
2020-10-19 18:30:06 -0400

    created classes and migrations

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,9 @@
-require 'bundler'
+require 'bundler/setup'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(
     adapter: 'sqlite3', 
-    database: 'db/development.db'
+    database: 'db/development.sqlite3'
     )
 
 require_all 'lib'
