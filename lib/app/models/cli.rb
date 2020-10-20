12b8ee57b99477111e842feac6b7818d67c0f4b7
PatriciaArnedo <paaarnedo@gmail.com>
2020-10-19 20:41:07 -0400

    added artii and TTY-Prompt gems

--- lib/app/models/cli.rb
+++ lib/app/models/cli.rb
@@ -1,4 +1,15 @@
+require "tty-prompt"
+require "pry"
+require "artii"
+
+
 class Cli < ActiveRecord::Base
-    
+    @@prompt = TTY::Prompt.new
+
+    def title_screen
+        system('clear')
+
+
+    end
 
 end
